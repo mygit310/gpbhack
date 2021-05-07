784e2584b40e493c02510acce13f006f0d34cd7c
Sergey <asvmail@gmail.com>
2021-05-07 21:39:08 +0300

    web интерфейс (черновик)
    выложен по адресу encm.ru

---
+++ web/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
+<head>
+ 
+ <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+ <meta name="yandex-verification" content="b589603625c1b4cb" />
+
+ <title>encm</title>
+
+ <link rel="stylesheet" href="index.css" type="text/css">
+
+ <link rel="shortcut icon" href="">
+
+ <link rel="alternate" href="" hreflang="en">
+ <link rel="alternate" href="" hreflang="ru">
+
+</head>
+<body>
+
+<div class="box">
+ <p><span style="font: 300% pragmatica,Helvetica,Arial,sans-serif; color: #000;">test</span></p>
+ <p><span style="font: 100% pragmatica-light,Helvetica,Arial,sans-serif; color: #77808b;">test</span></p>
+</div>
+
+<script></script>  
+
+</body>
+</html>
+
+
+﻿<?php
+
+/*
+ *
+ * Версия от 03.06.2017
+ *
+ *
+ */
+
+?>
